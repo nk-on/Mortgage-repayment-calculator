cac6e15be54189a22068f8415d6ccfedee9758b5
nk-on <2nkoz@proton.me>
2025-03-31 22:18:46 -0700

    Building and styling container

---
+++ src/Container.tsx
@@ -0,0 +1,5 @@
+export default function Container() {
+  return <>
+   <div className="w-[100%] h-[100%] md:w-[90%] md:h-[90%] lg:w-[1008px] lg:h-[606px] bg-[#FFFFFF] rounded-[30px]"></div>
+  </>;
+}
