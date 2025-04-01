7de86016d91423136a0887c01d17182ac9a9431e
nk-on <2nkoz@proton.me>
2025-03-31 22:52:21 -0700

    Adding and styling results container and form container

---
+++ src/FormContainer.tsx
@@ -0,0 +1,5 @@
+export default function FormContainer(){
+    return (
+        <div className="w-[50%] h-[100%] bg-[#fff] lg:rounded-tl-[50px] lg:rounded-bl-[50px] "></div>
+    )
+}
---
+++ src/ResultsContainer.tsx
@@ -0,0 +1,5 @@
+export default function ResultsContainer(){
+    return (
+        <div className="lg:w-[50%] h-[100%] w-[100%] bg-[#133041] lg:rounded-bl-[50px] lg:rounded-tr-[30px] rounded"></div>
+    )
+}
