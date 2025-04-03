2ac72af2da7617ca49efccea7bac833ca1d64187
nk-on <2nkoz@proton.me>
2025-04-02 22:07:02 -0700

    Style:adding and styling repayment button

--- src/FormContainer.tsx
+++
@@ -1,5 +0,0 @@
-export default function FormContainer(){
-    return (
-        <div className="w-[50%] h-[100%] bg-[#fff] lg:rounded-tl-[50px] lg:rounded-bl-[50px] "></div>
-    )
-}
--- src/FormContainer/FormContainer.tsx
+++ src/FormContainer/FormContainer.tsx
@@ -3,12 +3,12 @@ import Input from "./Input";
 import MortgageType from "./MortgageType";
 export default function FormContainer() {
   return (
-    <div className="flex flex-col  w-[50%] h-[100%] bg-[#fff] text-[#4E6E7E] lg:rounded-tl-[50px] lg:rounded-bl-[50px] lg:p-[30px] ">
+    <div className="flex flex-col lg:items-start items-center justify-between p-[30px] gap-[10px]  lg:h-[100%] h-[50%] bg-[#fff] text-[#4E6E7E] lg:rounded-tl-[50px] lg:rounded-bl-[50px] lg:p-[30px] ">
       <div className="w-[90%] lg:flex  justify-between items-center">
         <h1 className="text-[#133041] font-medium text-[25px]">Mortgage Calculator</h1>
         <p className=" underline">Clear All</p>
       </div>
-      <form className="flex flex-col justify-start w-[100%]">
+      <form className="flex flex-col gap-[30px] h-[100%] justify-start w-[100%]">
         <MortgageAmountInput />
         <div className="w-[100%] flex lg:flex-row flex-col gap-[50px]">
           <Input title = {"Mortgage term"} symbol = {"years"} />
@@ -22,6 +22,7 @@ export default function FormContainer() {
           </div>
         </div>
       </form>
+      <button className="bg-[#D8DB2F] flex justify-between items-center text-[#000] font-bold h-[56px] p-[30px] rounded-[50px] w-[70%]"><span><img src="src/assets/icon-calculator.svg" /></span>Calculate Repayments</button>
     </div>
   );
 }
