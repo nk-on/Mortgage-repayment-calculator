549c7307a5d6a3620067a18b581aa0708d765e83
nk-on <2nkoz@proton.me>
2025-04-08 23:08:16 -0700

    Feat:setting error messege on the mortgage amout input

--- src/FormContainer/FormContainer.tsx
+++ src/FormContainer/FormContainer.tsx
@@ -1,12 +1,21 @@
 import MortgageAmountInput from "./MortgageAmountInput";
 import Input from "./Input";
 import MortgageType from "./MortgageType";
-import { useContext,} from "react";
+import { useContext, useState } from "react";
 import { CalculatorContext } from "../CalculatorContext";
 import mortgageTypes from "./MortgageTypes";
 import mortgageInputs from "./MortgageInputs";
 export default function FormContainer() {
-  const { mortgageType,dispatch} = useContext(CalculatorContext);
+  const {
+    mortgageType,
+    dispatch,
+    mortgageAmount,
+    interestAmount,
+    yearsAmount,
+  } = useContext(CalculatorContext);
+  const [mortgageAmountError,setMortgageAmountError] = useState(false);
+  const [mortgageTermError,setMortgageTermError] = useState(false);
+  const [yearsAmoutnError,setYearsAmountError] = useState(false);
   return (
     <div className="flex flex-col lg:items-start items-center justify-between p-[30px] gap-[10px]  lg:h-[100%] h-[65%] bg-[#fff] text-[#4E6E7E] lg:rounded-tl-[50px] lg:rounded-bl-[50px] lg:p-[30px] ">
       <div className="w-[90%] lg:flex  justify-between items-center">
@@ -14,22 +23,35 @@ export default function FormContainer() {
         <p className=" underline">Clear All</p>
       </div>
       <form className="flex flex-col gap-[30px] h-[78%] justify-start w-[80%]">
-        <MortgageAmountInput />
+        <MortgageAmountInput error = {mortgageAmountError} setError = {setMortgageAmountError} />
         <div className="w-[100%] flex lg:flex-row flex-col gap-[50px]">
-          {mortgageInputs.map((element) => <Input title={element.title} symbol={element.symbol} />)}
+          {mortgageInputs.map((element,index) => (
+            <Input key={index} title={element.title} symbol={element.symbol} />
+          ))}
         </div>
         <div className="flex flex-col gap-[10px]">
           <h2>Motrgage type</h2>
           <div className="flex flex-col gap-[10px]">
-            {
-              mortgageTypes.map((element)=> <MortgageType key={element.id} title={element.title} id = {element.id} checked = {element.checked} />)
-            }
+            {mortgageTypes.map((element) => (
+              <MortgageType
+                key={element.id}
+                title={element.title}
+                id={element.id}
+                checked={element.checked}
+              />
+            ))}
           </div>
         </div>
       </form>
-      <button className="bg-[#D8DB2F] flex justify-between items-center text-[#000] font-bold lg:h-[56px] h-[18px] p-[30px] rounded-[50px] w-[70%]" onClick={()=>{
-         dispatch({type:mortgageType})
-      }}>
+      <button
+        className="bg-[#D8DB2F] flex justify-between items-center text-[#000] font-bold lg:h-[56px] h-[18px] p-[30px] rounded-[50px] w-[70%]"
+        onClick={() => {
+          if(mortgageAmount === 0){
+            setMortgageAmountError(true);
+          }
+          dispatch({ type: mortgageType });
+        }}
+      >
         <span>
           <img src="src/assets/icon-calculator.svg" />
         </span>
--- src/FormContainer/MortgageAmountInput.tsx
+++ src/FormContainer/MortgageAmountInput.tsx
@@ -1,18 +1,28 @@
-import { useContext } from "react"
-import { CalculatorContext } from "../CalculatorContext"
-export default function MortgageAmountInput(){
-    const {setMortgageAmount} = useContext(CalculatorContext);
-    return (
-        <div className="w-[100%]">
-          <h2 className="text-[#4E6E7E]">Mortgage Amount</h2> 
-          <div className="flex  border border-[#6B94A8] rounded-[5px]">
-            <div className="w-[44px] lg:h-[48px] h-[24px] flex justify-center items-center bg-[rgb(228,244,253)] rounded-tl-[5px] rounded-bl-[5px]">£</div>
-            <input type="number" className="w-[90%]  rounded-tr-[5px] rounded-br-[5px]" onChange={
-              (e)=>{
-                setMortgageAmount(Number(e.target.value));
-              }
-            } />
-          </div>
+import { useContext } from "react";
+import { CalculatorContext } from "../CalculatorContext";
+interface MortgageAmountInputProps {
+  error:boolean,
+  setError: React.Dispatch<React.SetStateAction<boolean>>
+}
+export default function MortgageAmountInput({error,setError}:MortgageAmountInputProps) {
+  const { setMortgageAmount } = useContext(CalculatorContext);
+  return (
+    <div className="w-[100%]">
+      <h2 className="text-[#4E6E7E]">Mortgage Amount</h2>
+      <div className="flex  border border-[#6B94A8] rounded-[5px]">
+        <div className={`w-[44px] lg:h-[48px] h-[24px] flex justify-center items-center ${error ? 'bg-[#D73328]':'bg-[rgb(228,244,253)]' } rounded-tl-[5px] rounded-bl-[5px]`}>
+          £
         </div>
-    )
+        <input
+          type="number"
+          className="w-[90%]  rounded-tr-[5px] rounded-br-[5px]"
+          onChange={(e) => {
+            setMortgageAmount(Number(e.target.value));
+            setError(false)
+          }}
+        />
+      </div>
+      {error && <div className="text-[#D73328]">This field is required</div>}
+    </div>
+  );
 }
